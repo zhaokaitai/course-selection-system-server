5914c225513654a1bf07cec98fbb27285c2f9bd6
qqz <1684182726@qq.com>
2024-10-04 23:10:04 +0800

    add 根据id获取教材

--- select-course/src/main/java/com/heub/selectcourse/controller/TeachingMaterialController.java
+++ select-course/src/main/java/com/heub/selectcourse/controller/TeachingMaterialController.java
@@ -1,8 +1,11 @@
 package com.heub.selectcourse.controller;
 
+import com.heub.selectcourse.model.domain.TeachingMaterial;
 import com.heub.selectcourse.service.TeachingMaterialService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TeachingMaterialController {
     @Resource
     private TeachingMaterialService teachingMaterialService;
+
+    @GetMapping("{id}")
+    public TeachingMaterial getTeachingMaterial(@PathVariable int id) {
+        return teachingMaterialService.getById(id);
+    }
 }
