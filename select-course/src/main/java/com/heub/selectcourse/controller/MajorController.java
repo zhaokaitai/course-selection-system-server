ba1aa704a23dae41cb371f6cb47066ce37cb773d
qqz <1684182726@qq.com>
2024-10-04 14:59:43 +0800

    add 根据id获取专业接口

--- select-course/src/main/java/com/heub/selectcourse/controller/MajorController.java
+++ select-course/src/main/java/com/heub/selectcourse/controller/MajorController.java
@@ -1,8 +1,11 @@
 package com.heub.selectcourse.controller;
 
+import com.heub.selectcourse.model.domain.Major;
 import com.heub.selectcourse.service.MajorService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MajorController {
     @Resource
     private MajorService majorService;
+
+    @GetMapping("{id}")
+    public Major getMajor(@PathVariable int id) {
+        return majorService.getById(id);
+    }
 }
