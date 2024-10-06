46a296b03f2ff46138fd2852b891c42c6fabab9e
qqz <1684182726@qq.com>
2024-10-06 16:14:22 +0800

    add 开启选课接口

--- select-course/src/main/java/com/heub/selectcourse/controller/TimesetController.java
+++ select-course/src/main/java/com/heub/selectcourse/controller/TimesetController.java
@@ -1,8 +1,12 @@
 package com.heub.selectcourse.controller;
 
+import com.heub.selectcourse.model.domain.Timeset;
+import com.heub.selectcourse.model.query.TimesetQuery;
 import com.heub.selectcourse.service.TimesetService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TimesetController {
     @Resource
     private TimesetService timesetService;
+
+    @PostMapping("on")
+    public boolean on(TimesetQuery timesetQuery) {
+        Timeset timeset = new Timeset();
+        BeanUtils.copyProperties(timesetQuery, timeset);
+        return timesetService.save(timeset);
+    }
+
 
 }
