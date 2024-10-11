eba979ce3762dba623dce8087f451d781dfd8d41
qqz <1684182726@qq.com>
2024-10-11 10:45:45 +0800

    fix

--- select-course/src/main/java/com/heub/selectcourse/controller/OperationRecordController.java
+++ select-course/src/main/java/com/heub/selectcourse/controller/OperationRecordController.java
@@ -14,6 +14,7 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,7 +35,7 @@ public class OperationRecordController {
 
 
     @GetMapping("page")
-    public BaseResponse<Page<OperationRecord>> page(RecordQuery recordQuery){
+    public BaseResponse<Page<OperationRecord>> page(@RequestBody RecordQuery recordQuery){
         if(recordQuery == null){
             throw  new BusinessException(ErrorCode.PARAMS_ERROR);
         }
